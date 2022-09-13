ab4351a06ea7d6b445099d8770b5bd219cf79a7b
Zane Othman-Gomez <zane.othman@gmail.com>
2022-09-12 18:25:32 -0700

    Rekey secrets with cryogonal keys

--- base/users/default.nix
+++ base/users/default.nix
@@ -10,6 +10,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOClmVzgghnkYd7nkLra97lhrdP+f524c3yz6uebBrjJ stone@solosis"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+3IqZFhMmrsjR67EnLwPDxfioaQ5r0ZXzA5/1IXR0I stone@simulacrum"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPZCD9GNDRshukMA6onTDPdzNGbeo/s0S9ZrqBR1AXn stone@spiritomb"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfzxRJsns5aoksFDBVIoL7u2StSPB+9kxQmY5ddnD+s stone@cryogonal"
     ];
   };
 
--- secrets.nix
+++ secrets.nix
@@ -3,13 +3,15 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOClmVzgghnkYd7nkLra97lhrdP+f524c3yz6uebBrjJ stone@solosis"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+3IqZFhMmrsjR67EnLwPDxfioaQ5r0ZXzA5/1IXR0I stone@simulacrum"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPZCD9GNDRshukMA6onTDPdzNGbeo/s0S9ZrqBR1AXn stone@spiritomb"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfzxRJsns5aoksFDBVIoL7u2StSPB+9kxQmY5ddnD+s stone@cryogonal"
   ];
+  cryogonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3dntUaHeHCAKZ/ki0SQ5e7Dvg602RseZvtR1R5XoQ1";
   solosis =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtnVjsCoTunr6ePI25yiRgzvHg5hOVsFEttGcA0wjsV";
   simulacrum =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICL9JQGalPr7Y4f1cez4PKhxNrdSp52/xGnQPXbyCJLC";
 in {
-  "secrets/wifi-psks.age".publicKeys = stone ++ [ solosis ];
+  "secrets/wifi-psks.age".publicKeys = stone ++ [ solosis cryogonal ];
 
   # solosis
   "machines/solosis/secrets/rsync-secrets.age".publicKeys = stone
