43faab2debdf029280fda9b003232f9223b8e2bd
Zane Othman <zane.othman@gmail.com>
2022-09-25 14:44:16 -0700

    (cryogonal) bump kernel version & get graphics working

--- machines/cryogonal/default.nix
+++ machines/cryogonal/default.nix
@@ -1,10 +1,17 @@
+{ config, ... }:
+
 {
   imports = [ ./hardware-configuration.nix ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
   boot.supportedFilesystems = [ "zfs" "ntfs" ];
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+  # Required to get graphics working
+  boot.extraModprobeConfig = "options i915 force_probe=46a6\n";
 
   networking.hostName = "cryogonal";
 
