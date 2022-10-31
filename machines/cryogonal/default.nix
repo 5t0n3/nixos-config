1f285bf8e74565ec1dea6d2fbb14d1bfb3b4c50e
Zane Othman <zane.othman@gmail.com>
2022-10-30 19:23:03 -0700

    (cryogonal) maybe fix x crashing?

--- machines/cryogonal/default.nix
+++ machines/cryogonal/default.nix
@@ -13,6 +13,7 @@
   # Required to get graphics working
   boot.extraModprobeConfig = ''
     options i915 force_probe=46a6
+    options i915 force_probe=5693
   '';
 
   networking.hostName = "cryogonal";
