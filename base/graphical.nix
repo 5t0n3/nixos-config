13f9be438fdcdcfc5b7b99df35da3a510c2ee147
Zane Othman <zane.othman@gmail.com>
2022-11-15 20:00:13 -0800

    (base) reenable nixpkgs-wayland overlay?

--- base/graphical.nix
+++ base/graphical.nix
@@ -69,7 +69,7 @@ in {
 
       # TODO: move into home-manager?
       nixpkgs.overlays = [
-        # inputs.nixpkgs-wayland.overlays.default
+        inputs.nixpkgs-wayland.overlays.default
         inputs.hyprpaper.overlays.default
         inputs.hyprland.overlays.default
       ];
