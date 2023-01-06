14f16dd020a2cee7c07e93b006dadf0ab1360908
Zane Othman <zane.othman@gmail.com>
2023-01-05 22:28:40 -0800

    (klefki) fail2ban: block both direct and proxied traffic on ban

--- machines/klefki/fail2ban.nix
+++ machines/klefki/fail2ban.nix
@@ -16,16 +16,18 @@
 
   services.fail2ban = {
     enable = true;
-    banaction-allports = "iptables-forward[type=allports]";
     jails.vaultwarden = ''
       enabled = true
       port = 8016
       backend = systemd
       filter = vaultwarden[journalmatch='_SYSTEMD_UNIT=vaultwarden.service']
-      banaction = %(banaction_allports)s
-      maxretry = 3
-      bantime = 14400
+      # Ban both proxied and direct traffic
+      banaction = iptables-forward[type=allports]
+                  iptables[type=allports]
+      # ban duration: 4 hours
       findtime = 14400
+      bantime = 14400
+      maxretry = 3
     '';
   };
 }
