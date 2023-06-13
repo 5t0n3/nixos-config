2eaaaa40422835d9c5bb859f6930e0c1e6e48f54
Zane Othman <zane.othman@gmail.com>
2023-06-13 15:02:15 -0700

    modify deploy script slightly

--- flake.nix
+++ flake.nix
@@ -134,24 +134,24 @@
       };
       text = ''
         # basic usage cause why not
-        if test $# -eq 0 || test "$1" = "--help"; then
+        if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
           echo "Usage: deploy <host> [-f]"
           echo "Deployment targets:"
           echo "${deployList}"
         # ensure a valid host was provided
-        elif echo "$1" | grep -v -E -q "${deployRegex}"; then
+        elif echo "$1" | grep -qvE "${deployRegex}"; then
           echo "Please specify a valid deployment host! Your choices are:"
           echo "${deployList}"
         # clean working tree deployment
         elif git diff-index --quiet HEAD; then
           revstr=$(git rev-parse --short HEAD)
-          echo "Deploying configuration version \"$revstr\" to \`$1\`!"
+          printf "Deploying configuration version \e[0;92m%s\e[0m to \e[1;97m%s\e[0m!" "$revstr" "$1"
           nixos-rebuild switch --target-host "$1.localdomain" --fast --use-remote-sudo --flake ".#$1" |& nom
         # Forced dirty deployment with fancy colors :)
-        elif test $# -eq 2 && test "$2" = "-f"; then 
-            printf "\e[1;91mWARNING!\e[0m"
-            echo " Deploying uncommitted configuration version to \`$1\`!"
-            nixos-rebuild switch --target-host "$1.localdomain" --fast --use-remote-sudo --flake ".#$1" |& nom
+        elif [ $# -eq 2 ] && [ "$2" = "-f" ]; then 
+          printf "\e[1;91mWARNING!\e[0m"
+          printf " Deploying uncommitted configuration version to \e[1;97m%s\e[0m!" "$1"
+          nixos-rebuild switch --target-host "$1.localdomain" --fast --use-remote-sudo --flake ".#$1" |& nom
         # dirty working tree warning
         else
           echo "Commit your changes before deploying! (or provide the -f flag)"
