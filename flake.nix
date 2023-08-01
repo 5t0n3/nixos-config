780e203b582135549d82c2f4ccd01110a39c92fb
Zane Othman <zane.othman@gmail.com>
2023-07-31 17:24:06 -0700

    bump flake inputs & unpin flake-utils (nothing uses it besides wsl)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
@@ -17,7 +16,6 @@
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     nix-index-db = {
       url = "github:Mic92/nix-index-database";
@@ -48,7 +46,6 @@
     pg-13 = {
       url = "github:5t0n3/pg-13";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
-      inputs.utils.follows = "flake-utils";
     };
   };
 
@@ -56,7 +53,6 @@
     self,
     nixpkgs,
     nixpkgs-unstable,
-    flake-utils,
     home-manager,
     home-manager-unstable,
     nix-index-db,
