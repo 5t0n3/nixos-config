7f88d05bbe230bd47bd5a142c98fd496ffa33bd1
Zane Othman <zane.othman@gmail.com>
2023-08-23 22:54:22 -0700

    (nacli) pov: you have made a grave error (typo)

--- machines/nacli/backups.nix
+++ machines/nacli/backups.nix
@@ -5,33 +5,34 @@
 }: let
   backup-python = pkgs.python3.withPackages (lib.attrVals ["cryptography" "b2sdk" "setuptools"]);
 in {
-  systemd.timers."vaultwarden-backup" = {
-    enable = true;
-    description = "Timer to trigger daily backup of Vaultwarden data";
-
-    after = ["network-online.target"];
-    wantedBy = ["multi-user.target"];
-
-    timerConfig = {
-      Persistent = true;
-      OnCalendar = "daily";
-      Unit = "vaultarden-backup.service";
-    };
-  };
-
   systemd.services."vaultwarden-backup" = {
     enable = true;
     description = "Vaultwarden backup to Backblaze B2";
 
+    wants = ["network-online.target"];
     after = ["network-online.target"];
     wantedBy = ["multi-user.target"];
 
     serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${backup-python}/bin/python ${./vw_backup.py}";
       User = "vaultwarden";
       WorkingDirectory = "/var/lib/bitwarden_rs";
       PrivateTmp = true;
       LoadCredential = "config.json:/var/lib/bitwarden_rs/backup-config.json";
-      ExecStart = "${backup-python}/bin/python ${./vw_backup.py}";
+    };
+  };
+
+  systemd.timers."vaultwarden-backup" = {
+    enable = true;
+    description = "Timer to trigger daily backup of Vaultwarden data";
+
+    wantedBy = ["timers.target"];
+
+    timerConfig = {
+      Persistent = true;
+      OnCalendar = "daily";
+      Unit = "vaultwarden-backup.service";
     };
   };
 }
