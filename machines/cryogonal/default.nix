305c95272230b689a79d38a57552abadbf83cffd
5t0n3 <31430937+5t0n3@users.noreply.github.com>
2023-10-07 10:15:52 -0700

    (cryogonal) back to podman but with docker compat

--- machines/cryogonal/default.nix
+++ machines/cryogonal/default.nix
@@ -71,22 +71,23 @@
 
   # we do a little virtualization (also man pages)
   virtualisation.libvirtd.enable = true;
-  environment.systemPackages = [pkgs.virt-manager inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.distrobox pkgs.man-pages pkgs.man-pages-posix pkgs.docker-compose]; # pkgs.podman-compose];
+  environment.systemPackages = [pkgs.virt-manager inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.distrobox pkgs.man-pages pkgs.man-pages-posix pkgs.docker-compose pkgs.podman-compose];
 
-  virtualisation.docker.enable = true;
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-  };
-
-  # virtualisation.podman.enable = true;
-  # virtualisation.containers.storage.settings = {
-  #   storage = {
-  #     driver = "zfs";
-  #     graphroot = "/var/lib/containers/storage";
-  #     runroot = "/run/containers/storage";
-  #   };
+  # virtualisation.docker.enable = true;
+  # virtualisation.docker.rootless = {
+  #   enable = true;
+  #   setSocketVariable = true;
   # };
+
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerSocket.enable = true;
+  virtualisation.containers.storage.settings = {
+    storage = {
+      driver = "zfs";
+      graphroot = "/var/lib/containers/storage";
+      runroot = "/run/containers/storage";
+    };
+  };
 
   documentation.dev.enable = true;
 
