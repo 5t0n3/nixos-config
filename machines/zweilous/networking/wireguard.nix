5fba8952d11ea347b305f3599b8480f11a8e8ac2
Zane <git@formulaic.cloud>
2024-05-02 19:58:50 -0700

    wireguard

--- machines/zweilous/networking/wireguard.nix
+++ machines/zweilous/networking/wireguard.nix
@@ -15,16 +15,31 @@
       ];
     };
 
+    # wg1 = {
+    #   address = ["10.100.1.53/32"];
+    #   dns = ["1.1.1.1"];
+    #   privateKeyFile = "/etc/sec.key";
+
+    #   peers = [
+    #     {
+    #       publicKey = "3AXD9MkrL03Tssx2K9tCiLGadHMGDhM3Z/hd1uYkcSQ=";
+    #       allowedIPs = ["10.100.0.0/16" "10.15.0.0/24"];
+    #       endpoint = "100.21.91.50:13338";
+    #       persistentKeepalive = 25;
+    #     }
+    #   ];
+    # };
+
     wg1 = {
-      address = ["10.100.1.53/32"];
+      address = ["10.100.2.47/32"];
       dns = ["1.1.1.1"];
       privateKeyFile = "/etc/sec.key";
 
       peers = [
         {
-          publicKey = "3AXD9MkrL03Tssx2K9tCiLGadHMGDhM3Z/hd1uYkcSQ=";
+          publicKey = "jIeRyIFz2yS/3lrn47IobKp5+DbWRpfYG+MfgRoKvWs=";
           allowedIPs = ["10.100.0.0/16" "10.15.0.0/24"];
-          endpoint = "100.21.91.50:13338";
+          endpoint = "100.21.91.50:13335";
           persistentKeepalive = 25;
         }
       ];
