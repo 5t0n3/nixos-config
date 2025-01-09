f5f78fdf67222c302d8d2be46b062af2d783c02e
Zane <git@formulaic.cloud>
2025-01-08 23:41:14 -0800

    didn't finish networking ig

--- machines/zweilous/networking.nix
+++ machines/zweilous/networking.nix
@@ -1,15 +1,22 @@
 {
   # networkmanager too
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = true;
+
+  # gotta be able to manage it
   users.users.stone.extraGroups = ["networkmanager"];
 
   # tailscaleeee
   services.tailscale.enable = true;
 
+  # exposing to local network or smth
   networking.firewall.allowedTCPPortRanges = [
     {
       from = 8000;
       to = 8010;
     }
   ];
+
+  # wireguard?
+  networking.firewall.allowedUDPPorts = [58666 58667];
 }
